fb860b629d2705709cbc0d2c7bc890ea535ebdbc
Kota <maedakoutaa@gmail.com>
2022-04-30 08:44:32 +0900

    Initial Commit

---
+++ MiniApp51-SideScrollingWithCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp51-SideScrollingWithCollectionView
+//
+//  Created by 前田航汰 on 2022/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
